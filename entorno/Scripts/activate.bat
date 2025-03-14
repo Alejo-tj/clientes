18e561e8cb4876322a7fa89520601acc64f908de
Alejo-tj <tintinagoalejo@gmail.com>
2025-03-14 17:03:38 -0500

    lista para despliegue

--- entorno/Scripts/activate.bat
+++ entorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\tinti\OneDrive\Desktop\adduzcan\entorno
+set VIRTUAL_ENV=C:\Users\tinti\OneDrive\Desktop\Helios\vacunas\entorno
 
 if not defined PROMPT set PROMPT=$P$G
 
